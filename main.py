0b4d5cb79a6833f3491706263c14dbb474fd7452
Hebert Jossue Toledo Valdovinos <hebert2508162@hybridge.education>
2026-06-21 15:44:16 -0600

    Add weather check and Twilio SMS notifications
    Integrate weather API and Twilio for notifications.

--- main.py
+++ main.py
@@ -1,38 +1,43 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
 import os
+from twilio.rest import Client
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+API_KEY = os.environ.get("OWM_API_KEY")
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+url = "https://api.openweathermap.org/data/2.5/forecast"
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+params = {
+    #"q": "Morelia,MX",
+    "lat": 20.039657,
+    "lon": -101.478589,
+    "appid": API_KEY,
+    "cnt": 4
+}
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+response = requests.get(url, params=params)
+response.raise_for_status()
+# print(response.text)
+weather_data = response.json()
+weather_id = weather_data['list'][0]['weather'][0]['id']
+weather_description = weather_data['list'][0]['weather'][0]['description']
+# print(weather_id)
+# print(weather_description)
+will_rain=False
+for item in weather_data['list'][:4]:
+    weather_id = item['weather'][0]['id']
+    if weather_id < 700:
+        will_rain = True
+        break
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body= "It's going to rain today. Remember to bring an umbrella",
+        from_ = '+19705781865',
+        to = '+524431463779'
+    )
+    print(message.status)
+else:
+    print("No rain expected.")
+    # print(item['weather'])
